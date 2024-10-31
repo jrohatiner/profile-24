6cb8adce5d0b9ddaefdf854829593292e943df92
jrohatiner <judithrohatiner@gmail.com>
2024-10-31 12:11:44 -0400

    first

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -22,14 +22,14 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
           </svg>
           {{ showPreview ? 'Hide CV' : 'View CV' }}
         </button>
-        <a [href]="cvUrl" download class="action-button download-button">
+       <!-- <a [href]="cvUrl" download class="action-button download-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
             <polyline points="7 10 12 15 17 10"></polyline>
             <line x1="12" y1="15" x2="12" y2="3"></line>
           </svg>
           Download CV
-        </a>
+        </a> -->
       </div>
 
       <div class="cv-preview" *ngIf="showPreview">
